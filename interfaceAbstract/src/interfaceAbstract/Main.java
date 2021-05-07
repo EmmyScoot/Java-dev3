6c9be4922aa775913d16143df0ffcef9bd7f8c12
x <x@x-Bilgisayar>
2021-05-07 21:24:32 -0100

    4.gün 2.ödev

--- interfaceAbstract/src/interfaceAbstract/Main.java
+++ interfaceAbstract/src/interfaceAbstract/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		BaseCustomerManager neroCustomerManager = new NeroCustomerManager();
-		Customer customer = new Customer(1, "Engin", "Demiroð", LocalDate.of(1985, 1, 6), "3333333333");
+		Customer customer = new Customer(1, "Engin", "Demiroð", LocalDate.of(2000, 2, 3), "3333333333");
 		neroCustomerManager.save(customer);
 
 		Customer customer1 = new Customer(1, "Emmy", "Scoot", LocalDate.of(2020, 1, 1), "11111111111");
